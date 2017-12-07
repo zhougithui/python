8573c699457af48c0de49b888c4d68d64969387b
周辉 <hui.zhou@lanmaoly.com>
2017-12-07 13:40:52 +0800

    a

---
+++ com/xsqt/py/demo1.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         for k in range(1, 5):
+#             if(i != j) and (j !=k) and (i != k):
+#                 print(i, j, k)
+
+# bonus1 = 100000 * 0.1
+# bonus2 = bonus1 + 100000 * 0.75
+# bonus4 = bonus2 + 200000 * 0.5
+# bonus6 = bonus4 + 200000 * 0.3
+# bonus10 = bonus6 + 400000 * 0.15
+#
+# i = int(input("input again:\n"))
+#
+# if i <= 100000:
+#     bonus = i * 0.1
+# else:
+#     bonus = 1
+#
+# print("bonus", bonus)
